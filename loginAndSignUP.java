a980e252c27affed7d2e29bd234be9a49fe8abc0
3m <Zeinab.Mostafa.554@h-eng.helwan.edu.eg>
2025-05-11 16:26:11 +0300

    register files

---
+++ loginAndSignUP.java
@@ -0,0 +1,17 @@
+
+package loginandsignup;
+
+public class loginAndSignUP {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login LoginFrame = new Login();
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null);
+         
+         
+    }
+    
+}
